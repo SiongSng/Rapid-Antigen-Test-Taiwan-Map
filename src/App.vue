d7bb96034e0723189da78356a0fc7f073dffe079
菘菘 <rrt467778@gmail.com>
2022-04-29 18:10:29 +0800

    feat: first feature

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,107 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <!-- 顯示藥局位置 -->
+    <div id="map"></div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { AxiosResponse } from "axios";
+import { Options, Vue } from "vue-class-component";
+
+import Leaflet from "leaflet";
+import { Feature, Point } from "geojson";
+let openStreetMap: Leaflet.Map;
 
 @Options({
-  components: {
-    HelloWorld,
+  components: {},
+  data: () => ({
+    data: [],
+    select: {},
+  }),
+  computed: {
+    pharmacies(): Feature<Point, { [name: string]: string }>[] {
+      return this.data;
+    },
+  },
+  watch: {
+    pharmacies() {
+      this.updateMap();
+    },
+  },
+  methods: {
+    updateMap() {
+      if (openStreetMap == null) return;
+
+      // clear map markers
+      openStreetMap.eachLayer((layer) => {
+        if (layer instanceof Leaflet.Marker) {
+          openStreetMap?.removeLayer(layer);
+        }
+      });
+
+      /// add map markers
+      this.pharmacies.forEach(
+        (pharmacy: Feature<Point, { [name: string]: string }>) => {
+          Leaflet.marker([
+            pharmacy.geometry.coordinates[1],
+            pharmacy.geometry.coordinates[0],
+          ]).addTo(openStreetMap)
+            .bindPopup(`<p><strong style="font-size: 20px;">${
+            pharmacy.properties.name
+          }</strong></p>
+           <strong style="font-size: 16px;">品牌: ${
+             pharmacy.properties.brand
+           }</br>
+          <strong style="font-size: 16px;">剩餘 ${
+            pharmacy.properties.count
+              ? `${pharmacy.properties.count} 份 (每份五個)`
+              : "未取得資料"
+          } 
+          </strong><br>
+          地址: ${pharmacy.properties.address}<br>
+          電話: ${pharmacy.properties.phone}<br>
+          備註: ${pharmacy.properties.note}<br>
+          <small>最後更新時間: ${pharmacy.properties.updated}</small>`);
+        }
+      );
+    },
+  },
+  mounted() {
+    let zoom = 16;
+    let maxZoom = 19;
+
+    openStreetMap = Leaflet.map("map", {
+      center: [25.042474, 121.513729],
+      zoom: zoom,
+    });
+    openStreetMap.locate({
+      setView: true,
+      maxZoom: zoom,
+    });
+
+    Leaflet.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: maxZoom,
+    }).addTo(openStreetMap);
+
+    const pharmacyAPIUrl =
+      "https://raw.githubusercontent.com/kiang/data.nhi.gov.tw/master/docs/antigen.json";
+    this.$http.get(pharmacyAPIUrl).then((response: AxiosResponse) => {
+      /// save pharmacy data
+      this.data = response.data.features;
+    });
   },
 })
 export default class App extends Vue {}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "bootstrap/scss/bootstrap";
+
+#map {
+  position: relative;
+  height: 98vh;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import Vue from 'vue';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+const app: Vue.App = createApp(App);
+app.use(VueAxios, axios);
+app.mount('#app');
+document.title = "Covid-19 家用抗原快篩地圖";
